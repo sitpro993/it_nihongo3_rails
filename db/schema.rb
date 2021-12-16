9daf7b7da5af74573adb8148775a520818054536
it_nihongo3_rails <sitpro993@gmail.com>
2021-12-16 03:52:56 +0000

    Add detail book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_05_141234) do
+ActiveRecord::Schema.define(version: 2021_12_16_034501) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
